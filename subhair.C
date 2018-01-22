3e7848dab476787c856f6afb08c3d950a14c1175
ssubhairmech1998 <35688062+ssubhairmech1998@users.noreply.github.com>
2018-01-22 16:44:07 +0530

    SUBHAIR
    POSITIVE

---
+++ subhair.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a;
+
+printf("enter a number less than 100000:\n");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the number is positive:");
+}
+else
+{
+printf("the number is negative:");
+}
+return 0;
+}
